6701d85f75332ec37ecd7b5c4261997c871c57a7
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-15 15:29:56 +0300

    create repo search function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
-from flask import render_template, request, redirect, url_for, abort
+from flask import render_template, redirect, url_for, abort, request
+
+from app.request import search_repositories
 from . import main
+
 #from .forms import exampleForm
 #from flask_login import login_required, current_user
 #from ..models import User
@@ -9,5 +12,24 @@ from . import main
 @main.route('/')
 def index():
     message = 'Test'
-    return render_template('index.html', message=message)
+    search_repos= request.args.get('repo_query')
+
+    if search_repos:
+      return redirect(url_for('.search', search_term = search_repos))
+
+  
+    return render_template('index.html')
+    
+
+@main.route('/search/<search_term>')
+def search(search_term):
+    '''
+    display search results
+    '''
+
+    search_term_list = search_term.split(" ") 
+    search_term_format = "+".join(search_term_list)
+    searched_repos = search_repositories(search_term_format)
+    print(searched_repos)
+    return render_template ('search.html', repos = searched_repos)
 
--- app/request.py
+++ app/request.py
@@ -13,9 +13,40 @@ def configure_request(app):
     global access_token
     access_token = app.config['ACCESS_TOKEN']
 
-def serach_repositories(search_term):
+def search_repositories(search_term):
     '''
     Function to return json response
     '''
-    search_repo_url = 'https://api.github.com/search/repositories?q={}/in:name'
+    search_repo_url = 'https://api.github.com/search/repositories?q={}/in:name'.format(search_term)
+    with urllib.request.urlopen(search_repo_url) as url:
+        search_repo_data = url.read()
+        search_repo_response = json.loads(search_repo_data)
+
+        search_repo_results = None
+
+        if search_repo_response['items']:
+            search_repo_list = search_repo_response['items']
+            search_repo_results = process_results(search_repo_list)
+
+    return search_repo_results
+
+def process_results(search_repo_list):
+    '''
+    process repo results
+    '''
+
+    search_results = []
+    for repo_item in search_repo_list:
+        html_url = repo_item.get('html_url')
+        owner = repo_item.get('owner.login')
+        description = repo_item.get('description')
+        language = repo_item.get('language')
+        language_url = repo_item.get('languages_url')
+
+        if html_url:
+            repo_object = Repository(html_url, owner, description, language, language_url)
+            search_results.append(repo_object)
+    print(search_results)
+    return search_results
+
 
